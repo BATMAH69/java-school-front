e6b811492ea4b05784b1e532f2c02ecaa9785347
batmah <dev-www@ya.ru>
2016-10-16 17:12:52 +0300

    init react

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  entry: './main.js',
+  output: {
+    path: './',
+    filename: 'index.js'
+  },
+  devServer:{
+    inline: true,
+    port:3333
+  },
+  module:{
+    loaders:[
+      {test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel'}
+    ]
+  }
+};
