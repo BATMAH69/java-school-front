72ceaf0d0e0a49e0eadd70c4312a6bcf2f8a1440
batmah <dev-www@ya.ru>
2016-10-19 14:35:11 +0300

    lesson16: Make app step 6 - extra file

--- components/App.js
+++ components/App.js
@@ -4,62 +4,11 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
+import Users from './Users';
+
 // import './style.css'
 
-const style = {
-  card: {
-    margin:10,
-    display: 'flex'
-  },
-  icon: {
-    marginRight: 10,
-    height: 40,
-    width: 40,
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: 'gold',
-    borderRadius: 20
-  },
-  column: {
-    display: 'flex',
-    flexDirection: 'column'
-  },
-  row: {
-    display: 'flex',
-    flexDirection: 'row'
-  },
-  description: {
-    width: 50,
-    color: 'gray'
-  }
-};
-
-
-const UsersItemRow = ({ description, value }) => (
-  <div style={style.row}>
-    <div style={style.description}>{description}:</div>
-    <div>{value}</div>
-  </div>
-);
-
-const UsersItem = ({ name, website }) => (
-  <div style={style.card}>
-    <div style={style.icon}>{name[0]}</div>
-    <div style={style.column}>
-      <UsersItemRow description="name" value={name} />
-      <UsersItemRow description="www" value={website} />
-    </div>
-  </div>
-);
-
-const Users = ({ users }) => (
-  <div>
-    {users.map(({ id, name, website }) => (
-      <UsersItem key={id} name={name} website={website} />
-    ))}
-  </div>
-);
+const style = {};
 
 class App extends Component {
   constructor(props) {
@@ -68,12 +17,17 @@ class App extends Component {
       userId: 0,
       users: null
     };
+    this.selectUser = this.selectUser.bind(this)
   }
 
   componentWillMount() {
     axios.get('https://jsonplaceholder.typicode.com/users')
       .then((response) => this.setState({ users: response.data }))
       .catch((errror) => console.error(errror))
+  }
+
+  selectUser(userId){
+    this.setState({userId});
   }
 
   render() {
