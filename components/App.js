e5cf577e1c6ec6e5ebcbbf28bf59852c0e4c9bf4
batmah <dev-www@ya.ru>
2016-10-19 12:53:22 +0300

    lesson16: Make app step 3 - add UserItem

--- components/App.js
+++ components/App.js
@@ -4,6 +4,52 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
+// import './style.css'
+
+const style = {
+  card: {
+    display: 'flex'
+  },
+  icon: {
+    marginRight: 10,
+    height: 40,
+    width: 40,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'gold',
+    borderRadius: 20
+  },
+  column: {
+    display: 'flex',
+    flexDirection: 'column'
+  },
+  row: {
+    display: 'flex',
+    flexDirection: 'row'
+  },
+  description:{
+    width: 50
+  }
+};
+
+
+const UsersItem = ({ id, name, website }) => (
+  <div style={style.card} key={id}>
+    <div style={style.icon}>{name[0]}</div>
+    <div style={style.column}>
+      <div style={style.row}>
+        <div style={style.description}>name:</div>
+        <div>{name}</div>
+      </div>
+      <div style={style.row}>
+        <div style={style.description}>www:</div>
+        <div>{website}</div>
+      </div>
+    </div>
+  </div>
+);
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -15,13 +61,13 @@ class App extends Component {
 
   componentWillMount() {
     axios.get('https://jsonplaceholder.typicode.com/users')
-      .then((response) => this.setState({users: response.data}))
+      .then((response) => this.setState({ users: response.data }))
       .catch((errror) => console.error(errror))
   }
 
   render() {
     console.log(this.state.users);
-    if (!this.state.users){
+    if (!this.state.users) {
       return (
         <div>
           Loading...
@@ -31,17 +77,8 @@ class App extends Component {
 
     return (
       <div>
-        {this.state.users.map(({id, name, website}) => (
-          <div key={id}>
-            <div>
-              <div>name:</div>
-              <div>{name}</div>
-            </div>
-            <div>
-              <div>www:</div>
-              <div>{website}</div>
-            </div>
-          </div>
+        {this.state.users.map(({ id, name, website }) => (
+          <UsersItem key={id} name={name} website={website} />
         ))}
       </div>
     )
