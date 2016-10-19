296192c3754be6bde73c89712c5b1cf05a86bbee
batmah <dev-www@ya.ru>
2016-10-19 10:42:21 +0300

    lesson16: Make app step 2 - get users

--- components/App.js
+++ components/App.js
@@ -2,11 +2,21 @@
  * Created by batmah on 19.10.16.
  */
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class App extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      userId: 0,
+      users: null
+    };
+  }
 
+  componentWillMount() {
+    axios.get('https://jsonplaceholder.typicode.com/users')
+      .then((response) => this.setState({users: response.data}))
+      .catch((errror) => alert('Всё плохо'))
   }
 
   render() {
