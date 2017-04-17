daeb8a2bf585082752889dc9378aa5d061b2c4f5
batmah <dev-www@ya.ru>
2017-04-17 04:02:20 +0300

    redux add devtools

--- main.js
+++ main.js
@@ -3,14 +3,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
-import { createStore, combineReducers } from 'redux';
+import { createStore, compose, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
 
 import app from './components/App/reducer';
 
 const reducer = combineReducers({ app });
 
-const store = createStore(reducer);
+const createStoreWithDevTools = compose(
+  global.devToolsExtension ? global.devToolsExtension() : f => f,
+)(createStore);
 
+const store = createStoreWithDevTools(reducer);
 
 ReactDOM.render(<Provider store={store}><App target="react" /></Provider>, document.getElementById('app'));
