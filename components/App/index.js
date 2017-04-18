6e255baec3aaf38cd44312c83d8da6bcff3c950e
batmah <dev-www@ya.ru>
2017-04-19 00:25:32 +0300

    redux undo point connect

--- components/App/index.js
+++ components/App/index.js
@@ -10,29 +10,27 @@ import { setCount, renewCount } from './actions'
 const Input = (props) => <input {...props} />;
 const Div = (props) => <div {...props} />;
 
-const Count = connect(
-  store => ({count: store.app.count}),
-  dispatch => ({ renewCount: () => dispatch(renewCount) })
-)((props) => <Div>{props.count}</Div>);
-
-const CountInput = connect(
-  store => ({count: store.app.count}),
-  dispatch => ({ setCount: event => dispatch(setCount(event.target.value)) })
-)((props) => <Input value={props.count} onChange={props.setCount} />);
-
-
 const App = (props) => (
   <Div>
     <Div onClick={props.renewCount}>
-      <Count />
       <Div>Hi {props.target} VERY BIG ELEMENT</Div>
+      <Div>{props.count}</Div>
     </Div>
-    <CountInput value={props.count} onChange={props.setCount} />
+    <Input value={props.count} onChange={props.setCount} />
   </Div>
 );
 
 App.propTypes = {
   target: React.PropTypes.string.isRequired,
+  count: React.PropTypes.string.isRequired,
+  setCount: React.PropTypes.func.isRequired
 };
 
-export default App;
+export default connect(
+  store => ({count: store.app.count}),
+  dispatch => ({
+    setCount: event => dispatch(setCount(event.target.value)),
+    renewCount: () => dispatch(renewCount),
+
+  })
+)(App);
