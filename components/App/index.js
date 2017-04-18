34edcbe814deb005fa6326bd74eb48c4036b4d2a
batmah <dev-www@ya.ru>
2017-04-19 01:44:55 +0300

    redux solve rerender

--- components/App/index.js
+++ components/App/index.js
@@ -39,26 +39,9 @@ export default connect(
   store => ({
     count0: store.app.count0,
     count1: store.app.count1,
-    count2: store.app.count2,
-    count3: store.app.count3,
-    count4: store.app.count4,
-    count5: store.app.count5,
-    count6: store.app.count6,
-    count7: store.app.count7,
-    count8: store.app.count8,
-    count9: store.app.count9,
+
   }),
   dispatch => ({
-    setCount0: event => dispatch(setCount0(event.target.value)),
-    setCount1: event => dispatch(setCount1(event.target.value)),
-    setCount2: event => dispatch(setCount2(event.target.value)),
-    setCount3: event => dispatch(setCount3(event.target.value)),
-    setCount4: event => dispatch(setCount4(event.target.value)),
-    setCount5: event => dispatch(setCount5(event.target.value)),
-    setCount6: event => dispatch(setCount6(event.target.value)),
-    setCount7: event => dispatch(setCount7(event.target.value)),
-    setCount8: event => dispatch(setCount8(event.target.value)),
-    setCount9: event => dispatch(setCount9(event.target.value)),
     renewCount: () => dispatch(renewCount),
   })
 )(App);
