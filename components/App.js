ce479c91ceb09ae1bf263ee72b5bfa01b07be71b
batmah <dev-www@ya.ru>
2017-04-26 14:33:27 +0300

    immutable set problem

--- components/App.js
+++ components/App.js
@@ -2,22 +2,24 @@
  * Created by batmah on 16.10.16.
  */
 import React from 'react';
-import { fromJS, Record } from 'immutable';
+import { fromJS } from 'immutable';
 
+const a = fromJS({b:[1,{c:25},3,4,5], d:6});
 
-const ARecord = Record({b:[1,{c:25},3,4,5], d:6});
-const a = new ARecord();
+const c = 25;
 
-const c = 5;
-
-const e = a.setIn(['d'], c);
+const e = a.setIn(['b',1,'c'], 2).setIn(['b',1,'c'], c);
 
 // arrow
 const App = () => {
   console.log(a);
   console.log(e);
   console.log('e === a', e === a);
-  console.log('e.d === a.d',e.d === a.d);
+  console.log('e.d === a.d',e.get('d') === a.get('d'));
+  console.log('e.d === a.d',e.get('b') === a.get('b'));
+  console.log('e.b[0] === a.b[0]',e.getIn(['b',0]) === a.getIn(['b',0]));
+  console.log('e.b[1] === a.b[1]',e.getIn(['b',1]) === a.getIn(['b',1]));
+  console.log('e.b[1].c === a.b[1].c',e.getIn(['b',1,'c']) === a.getIn(['b',1,'c']));
 
   return <div>ALL IN CONSOLE</div>
 };
