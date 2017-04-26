3f3b0af708b36f17279409d549db66fb2bd8b333
batmah <dev-www@ya.ru>
2017-04-26 11:03:12 +0300

    Bad solution

--- components/App.js
+++ components/App.js
@@ -8,15 +8,28 @@ const a = {b:[1,{c:25},3,4,5], d:6};
 
 const c = 24;
 
-const e = a;
+// const e = a;
+//
+// e.b[1].c = c;
 
-e.b[1].c = c;
+const b1 = Object.assign({}, a.b[1], { c });
+
+const b = a.b.slice();
+b.splice(1,1,b1);
+
+const e = Object.assign({}, a, { b });
 
 // arrow
 const App = () => {
   console.log(a);
   console.log(e);
-  console.log(e === a);
+  console.log('e === a', e === a);
+  console.log('e.d === a.d',e.d === a.d);
+  console.log('e.b === a.b',e.b === a.b);
+  console.log('e.b[0] === a.b[0]',e.b[0] === a.b[0]);
+  console.log('e.b[1] === a.b[1]',e.b[1] === a.b[1]);
+  console.log('e.b[1].z === a.b[1].z',e.b[1].z === a.b[1].z);
+
   return <div>ALL IN CONSOLE</div>
 };
 
