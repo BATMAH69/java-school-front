75e4051ca424f40bab92ae0c55ec09811620f7e1
batmah <dev-www@ya.ru>
2017-04-26 11:36:17 +0300

    immutable solution 24

--- components/App.js
+++ components/App.js
@@ -4,7 +4,7 @@
 import React from 'react';
 import { fromJS } from 'immutable';
 
-const a = {b:[1,{c:25},3,4,5], d:6};
+const a = fromJS({b:[1,{c:25},3,4,5], d:6});
 
 const c = 24;
 
@@ -12,32 +12,27 @@ const c = 24;
 //
 // e.b[1].c = c;
 
-// const b1 = Object.assign({}, a.b[1], { c });
-//
-// const b = a.b.slice();
-// b.splice(1,1,b1);
-//
-// const e = Object.assign({}, a, { b });
+const e = a.setIn(['b',1,'c'], c);
 
-const b1 = (a.b[1].c === c)? a.b[1] : Object.assign({}, a.b[1], { c });
-
-let b = a.b;
-if (a.b[1] !== b1){
-  b = a.b.slice();
-  b.splice(1,1,b1);
-}
-const e = (a.b === b)? a : Object.assign({}, a, { b });
+// const b1 = (a.b[1].c === c)? a.b[1] : Object.assign({}, a.b[1], { c });
+//
+// let b = a.b;
+// if (a.b[1] !== b1){
+//   b = a.b.slice();
+//   b.splice(1,1,b1);
+// }
+// const e = (a.b === b)? a : Object.assign({}, a, { b });
 
 // arrow
 const App = () => {
   console.log(a);
   console.log(e);
   console.log('e === a', e === a);
-  console.log('e.d === a.d',e.d === a.d);
-  console.log('e.b === a.b',e.b === a.b);
-  console.log('e.b[0] === a.b[0]',e.b[0] === a.b[0]);
-  console.log('e.b[1] === a.b[1]',e.b[1] === a.b[1]);
-  console.log('e.b[1].z === a.b[1].z',e.b[1].c === a.b[1].c);
+  // console.log('e.d === a.d',e.d === a.d);
+  // console.log('e.b === a.b',e.b === a.b);
+  // console.log('e.b[0] === a.b[0]',e.b[0] === a.b[0]);
+  // console.log('e.b[1] === a.b[1]',e.b[1] === a.b[1]);
+  // console.log('e.b[1].z === a.b[1].z',e.b[1].c === a.b[1].c);
 
   return <div>ALL IN CONSOLE</div>
 };
