7de1e7fe632402db44ec6455e88c643aaa27226a
batmah <dev-www@ya.ru>
2017-04-26 11:42:15 +0300

    immutable problem

--- components/App.js
+++ components/App.js
@@ -8,31 +8,18 @@ const a = fromJS({b:[1,{c:25},3,4,5], d:6});
 
 const c = 25;
 
-// const e = a;
-//
-// e.b[1].c = c;
-
 const e = a.setIn(['b',1,'c'], c);
-
-// const b1 = (a.b[1].c === c)? a.b[1] : Object.assign({}, a.b[1], { c });
-//
-// let b = a.b;
-// if (a.b[1] !== b1){
-//   b = a.b.slice();
-//   b.splice(1,1,b1);
-// }
-// const e = (a.b === b)? a : Object.assign({}, a, { b });
 
 // arrow
 const App = () => {
   console.log(a);
   console.log(e);
   console.log('e === a', e === a);
-  // console.log('e.d === a.d',e.d === a.d);
-  // console.log('e.b === a.b',e.b === a.b);
-  // console.log('e.b[0] === a.b[0]',e.b[0] === a.b[0]);
-  // console.log('e.b[1] === a.b[1]',e.b[1] === a.b[1]);
-  // console.log('e.b[1].z === a.b[1].z',e.b[1].c === a.b[1].c);
+  console.log('e.d === a.d',e.d === a.d);
+  console.log('e.b === a.b',e.b === a.b);
+  console.log('e.b[0] === a.b[0]',e.b[0] === a.b[0]);
+  console.log('e.b[1] === a.b[1]',e.b[1] === a.b[1]);
+  console.log('e.b[1].z === a.b[1].z',e.b[1].c === a.b[1].c);
 
   return <div>ALL IN CONSOLE</div>
 };
