802442fda1bd078f9e955bd0fdf728f1487d0e12
batmah <dev-www@ya.ru>
2017-05-03 13:36:56 +0300

    18. setup router

--- main.js
+++ main.js
@@ -3,5 +3,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
 
-ReactDOM.render(<App target="react" />, document.getElementById('app'));
+
+ReactDOM.render(
+  <Router>
+    <div>
+      <Link to="/user/1" >user1</Link>
+      <Link to="/users" >users</Link>
+      <Link to="/userswqewqe" >usersadas</Link>
+      <Link to="/" >usersadas</Link>
+      <Route exact path="/user/:id" component={App} />
+      <Route exact path="/" component={App} />
+    </div>
+  </Router>, document.getElementById('app'));
