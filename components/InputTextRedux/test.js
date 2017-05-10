5b8c7b08efc4e331f9d54937dcbb39c24b1f53e6
batmah <dev-www@ya.ru>
2017-05-10 14:14:53 +0300

    26. add describe

--- components/InputTextRedux/test.js
+++ components/InputTextRedux/test.js
@@ -5,11 +5,21 @@ import reducers from './reducer';
 
 const filterNode = (state) => fromJS(state.app);
 
-it('reducers', () => {
-  const prevState = filterNode({app:{}});
-  const action = {type:'SET_COUNT_COUNT0',payload:'1',node:'count0'};
+describe('InputTextRedux', () => {
 
+  const prevState = filterNode({ app: {} });
+  const action = { type: 'SET_COUNT_COUNT0', payload: '1', node: 'count0' };
   const nextState = reducers(prevState, action);
-  const state = filterNode({app:{count0:'1'}});
-  expect(true).toEqual(state.equals(nextState));
+
+
+
+  it('reducers', () => {
+
+    const state = filterNode({ app: { count0: '1' } });
+    expect(true).toEqual(nextState.equals(state));
+
+  });
+
+
 });
+
