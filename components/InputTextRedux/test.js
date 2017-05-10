5ccc6181a7bab4168541ab5ce3590e392c6a99ff
batmah <dev-www@ya.ru>
2017-05-10 13:57:18 +0300

    24. reducer test

--- components/InputTextRedux/test.js
+++ components/InputTextRedux/test.js
@@ -1,4 +1,15 @@
+// Эти данные скопированиы с консоли
 
-it('1=1', () => {
-  expect(true).toEqual(1 === 1);
+import {fromJS} from 'immutable';
+import reducers from './reducer';
+
+const filterNode = (state) => fromJS(state.app);
+
+it('reducers', () => {
+  const prevState = filterNode({app:{}});
+  const action = {type:'SET_COUNT_COUNT0',payload:'1',node:'count0'};
+
+  const nextState = reducers(prevState, action);
+  const state = filterNode({app:{count0:'1'}});
+  expect(true).toEqual(state.equals(nextState));
 });
