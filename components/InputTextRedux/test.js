7525d6c39af297bfea508f879d2e727a8ba2d62c
batmah <dev-www@ya.ru>
2017-05-10 14:53:52 +0300

    27. action test

--- components/InputTextRedux/test.js
+++ components/InputTextRedux/test.js
@@ -1,7 +1,14 @@
 // Эти данные скопированиы с консоли
 
+import React from 'react';
+import ReactDOM from 'react-dom';
 import {fromJS} from 'immutable';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import TestUtils from  'react-addons-test-utils';
+import InputTextRedux from './index';
 import reducers from './reducer';
+
 
 const filterNode = (state) => fromJS(state.app);
 
@@ -11,7 +18,25 @@ describe('InputTextRedux', () => {
   const action = { type: 'SET_COUNT_COUNT0', payload: '1', node: 'count0' };
   const nextState = reducers(prevState, action);
 
+  it('action', () => {
 
+    const storePrev = createStore(state => state, prevState);
+
+    const dispatch = jasmine.createSpy('dispatch')
+    storePrev.dispatch = dispatch;
+
+    const node = TestUtils.renderIntoDocument(
+      <Provider store={storePrev}>
+        <InputTextRedux name={'count0'} />
+      </Provider>
+    );
+
+    const elem = ReactDOM.findDOMNode(node).querySelector('input');
+
+    TestUtils.Simulate.change(elem, {target: {value: '1'}});
+    expect(dispatch).toHaveBeenCalledWith(action)
+
+  });
 
   it('reducers', () => {
 
@@ -19,7 +44,6 @@ describe('InputTextRedux', () => {
     expect(true).toEqual(nextState.equals(state));
 
   });
-
 
 });
 
