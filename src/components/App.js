5932dd2927dba418584742cb7a9c738b7f050624
batmah <dev-www@ya.ru>
2019-06-24 14:27:41 +0300

    lesson5: refs

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import { string, func } from 'prop-types'
 
-const Widget = (props) => (
+const Widget = ({ target, message, handlerUpdate }) => (
   <div>
-    <div>Hi {props.target} {props.message}</div>
-    <input onChange={props.handlerUpdate} />
+    <div>Hi {target} {message}</div>
+    <input onChange={handlerUpdate} />
   </div>
 );
 
@@ -22,33 +23,30 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      text: ''
+      text: '',
+      width: 0
     };
     this.update = this.update.bind(this);
+    this.widgetRef = React.createRef();
   }
 
   update(e) {
-    this.setState({ text: e.target.value })
+    console.log();
+    this.setState({
+      text: e.target.value,
+      width: ReactDOM.findDOMNode(this.widgetRef.current).offsetWidth
+    })
   }
 
   render() {
     return (
-      <div>
+      <div ref={this.widgetRef} style={{ display: 'inline-block' }}>
         <Widget
           target={this.props.target}
           message={this.state.text}
           handlerUpdate={this.update}
         />
-        <Widget
-          target={this.props.target}
-          message={this.state.text}
-          handlerUpdate={this.update}
-        />
-        <Widget
-          target={this.props.target}
-          message={this.state.text}
-          handlerUpdate={this.update}
-        />
+        <div>{this.state.width}</div>
       </div>
     );
   }
