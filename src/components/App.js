9781d68e1eb797e9c8ca2b4e8a1a81410bbf0284
batmah <dev-www@ya.ru>
2019-06-24 14:12:20 +0300

    lesson1:  hello world

---
+++ src/components/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+// //origin js
+// const App = function App() {
+//   return React.createElement(
+//     'div',
+//     null,
+//     'Hi World1'
+//   );
+// };
+
+// // class
+// class App extends React {
+//   render() {
+//     return (
+//       <div>Hi World</div>
+//     )
+//   }
+// }
+
+// arrow
+const App = () => (
+  <div>Hi World</div>
+);
+
+export default App;
