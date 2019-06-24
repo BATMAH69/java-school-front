9f9b4b2c33b957386cc108f5e92a98f3d05c80d2
batmah <dev-www@ya.ru>
2019-06-24 15:50:11 +0300

    lesson16: Make app step 6 - extra file

--- src/components/App.js
+++ src/components/App.js
@@ -1,60 +1,9 @@
 import React, { Component } from 'react';
+import Users from './Users';
 
 // import './style.css'
 
-const style = {
-  card: {
-    margin:10,
-    display: 'flex'
-  },
-  icon: {
-    marginRight: 10,
-    height: 40,
-    width: 40,
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: 'gold',
-    borderRadius: 20
-  },
-  column: {
-    display: 'flex',
-    flexDirection: 'column'
-  },
-  row: {
-    display: 'flex',
-    flexDirection: 'row'
-  },
-  description:{
-    width: 50,
-    color: 'gray',
-  }
-};
-
-const UsersItemRow = ({ description, value }) => (
-  <div style={style.row}>
-    <div style={style.description}>{description}:</div>
-    <div>{value}</div>
-  </div>
-);
-
-const UsersItem = ({ id, name, website }) => (
-  <div style={style.card} key={id}>
-    <div style={style.icon}>{name[0]}</div>
-    <div style={style.column}>
-      <UsersItemRow description="name" value={name} />
-      <UsersItemRow description="www" value={website} />
-    </div>
-  </div>
-);
-
-const Users = ({ users }) => (
-  <div>
-    {users.map(({ id, name, website }) => (
-      <UsersItem key={id} name={name} website={website} />
-    ))}
-  </div>
-);
+const style = {};
 
 class App extends Component {
   constructor(props) {
@@ -71,7 +20,10 @@ class App extends Component {
       .then(users => this.setState({users}))
       .catch(err => alert(`Всё плохо ${err}`))
   }
-
+  
+  selectUser(userId){
+    this.setState({userId});
+  }
 
   render() {
     console.log(this.state.users);
---
+++ src/components/Users.js
@@ -0,0 +1,61 @@
+/**
+ * Created by batmah on 19.10.16.
+ */
+import React from 'react';
+
+const style = {
+  card: {
+    margin:10,
+    display: 'flex'
+  },
+  icon: {
+    marginRight: 10,
+    height: 40,
+    width: 40,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'gold',
+    borderRadius: 20
+  },
+  column: {
+    display: 'flex',
+    flexDirection: 'column'
+  },
+  row: {
+    display: 'flex',
+    flexDirection: 'row'
+  },
+  description: {
+    width: 50,
+    color: 'gray'
+  }
+};
+
+
+const UsersItemRow = ({ description, value }) => (
+  <div style={style.row}>
+    <div style={style.description}>{description}:</div>
+    <div>{value}</div>
+  </div>
+);
+
+const UsersItem = ({ name, website }) => (
+  <div style={style.card}>
+    <div style={style.icon}>{name[0]}</div>
+    <div style={style.column}>
+      <UsersItemRow description="name" value={name} />
+      <UsersItemRow description="www" value={website} />
+    </div>
+  </div>
+);
+
+const Users = ({ users }) => (
+  <div>
+    {users.map(({ id, name, website }) => (
+      <UsersItem key={id} name={name} website={website} />
+    ))}
+  </div>
+);
+
+export default Users;
