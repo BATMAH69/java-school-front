30fbf33330d6b6657f8e8af0039f6f9034f719bd
batmah <dev-www@ya.ru>
2019-06-24 15:39:24 +0300

    lesson16: Make app step 2 - get users

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,19 @@ import React, { Component } from 'react';
 class App extends Component {
   constructor(props) {
     super(props);
-
+    this.state = {
+      userId: 0,
+      users: null
+    };
   }
+
+  componentWillMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({users}))
+      .catch(err => alert(`Всё плохо ${err}`))
+  }
+
 
   render() {
     return (
