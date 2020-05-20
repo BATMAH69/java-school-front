2616a077a1c984eb79a32b7335d417906d844c29
batmah <dev-www@ya.ru>
2020-05-20 18:11:25 +0300

    redux problem solve 16

--- components/App/index.js
+++ components/App/index.js
@@ -5,70 +5,59 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import {
-  setCount0,
-  setCount1,
-  setCount2,
-  setCount3,
-  setCount4,
-  setCount5,
-  setCount6,
-  setCount7,
-  setCount8,
-  setCount9,
+  setCount,
   renewCount
 } from './actions'
 
 //Для отображения обновлений
 const Input = (props) => <input {...props} />;
 const Div = (props) => <div {...props} />;
+const Span = (props) => <span {...props} />;
+
+const style = {margin: 10};
+
+const Counts = connect(
+  store => ({
+    count0: store.app.count0,
+    count1: store.app.count1,
+  })
+)((props) => <Div>{props.count0+' '+props.count1}</Div>);
+
+const InputText = (props) => (
+  <Span><Input style={style} value={props.count} onChange={props.setCount} /></Span>
+);
+
+const InputTextRedux = connect(
+  (store, props) => ({
+    count: store.app[props.name],
+  }),
+  (dispatch, props) => ({
+    setCount: event => dispatch(setCount(event.target.value, props.name)),
+  })
+)(InputText);
 
 const App = (props) => (
   <Div>
     <Div onClick={props.renewCount}>
       <Div>Hi {props.target} VERY BIG ELEMENT</Div>
-      <Div>{props.count0+' '+props.count1}</Div>
+      <Counts />
     </Div>
-    <Input value={props.count0} onChange={props.setCount0} />
-    <Input value={props.count1} onChange={props.setCount1} />
-    <Input value={props.count2} onChange={props.setCount2} />
-    <Input value={props.count3} onChange={props.setCount3} />
-    <Input value={props.count4} onChange={props.setCount4} />
-    <Input value={props.count5} onChange={props.setCount5} />
-    <Input value={props.count6} onChange={props.setCount6} />
-    <Input value={props.count7} onChange={props.setCount7} />
-    <Input value={props.count8} onChange={props.setCount8} />
-    <Input value={props.count9} onChange={props.setCount9} />
+    <InputTextRedux name={'count0'} />
+    <InputTextRedux name={'count1'} />
+    <InputTextRedux name={'count2'} />
+    <InputTextRedux name={'count3'} />
+    <InputTextRedux name={'count4'} />
+    <InputTextRedux name={'count5'} />
+    <InputTextRedux name={'count6'} />
+    <InputTextRedux name={'count7'} />
+    <InputTextRedux name={'count8'} />
+    <InputTextRedux name={'count9'} />
   </Div>
 );
 
-App.propTypes = {
-  target: React.PropTypes.string.isRequired
-};
-
 export default connect(
-  store => ({
-    count0: store.app.count0,
-    count1: store.app.count1,
-    count2: store.app.count2,
-    count3: store.app.count3,
-    count4: store.app.count4,
-    count5: store.app.count5,
-    count6: store.app.count6,
-    count7: store.app.count7,
-    count8: store.app.count8,
-    count9: store.app.count9,
-  }),
+  null,
   dispatch => ({
-    setCount0: event => dispatch(setCount0(event.target.value)),
-    setCount1: event => dispatch(setCount1(event.target.value)),
-    setCount2: event => dispatch(setCount2(event.target.value)),
-    setCount3: event => dispatch(setCount3(event.target.value)),
-    setCount4: event => dispatch(setCount4(event.target.value)),
-    setCount5: event => dispatch(setCount5(event.target.value)),
-    setCount6: event => dispatch(setCount6(event.target.value)),
-    setCount7: event => dispatch(setCount7(event.target.value)),
-    setCount8: event => dispatch(setCount8(event.target.value)),
-    setCount9: event => dispatch(setCount9(event.target.value)),
     renewCount: () => dispatch(renewCount),
   })
 )(App);
--- components/App/reducer.js
+++ components/App/reducer.js
@@ -1,14 +1,5 @@
 import {
-  SET_COUNT0,
-  SET_COUNT1,
-  SET_COUNT2,
-  SET_COUNT3,
-  SET_COUNT4,
-  SET_COUNT5,
-  SET_COUNT6,
-  SET_COUNT7,
-  SET_COUNT8,
-  SET_COUNT9,
+  SET_COUNT,
   RENEW_COUNT
 } from './actions';
 import {reducerObjective} from '../../helper/redux';
@@ -16,16 +7,7 @@ import {reducerObjective} from '../../helper/redux';
 const initialState = {};
 
 const reducers = {
-  [SET_COUNT0]: (state, action) => Object.assign({}, state, { count0: action.payload }),
-  [SET_COUNT1]: (state, action) => Object.assign({}, state, { count1: action.payload }),
-  [SET_COUNT2]: (state, action) => Object.assign({}, state, { count2: action.payload }),
-  [SET_COUNT3]: (state, action) => Object.assign({}, state, { count3: action.payload }),
-  [SET_COUNT4]: (state, action) => Object.assign({}, state, { count4: action.payload }),
-  [SET_COUNT5]: (state, action) => Object.assign({}, state, { count5: action.payload }),
-  [SET_COUNT6]: (state, action) => Object.assign({}, state, { count6: action.payload }),
-  [SET_COUNT7]: (state, action) => Object.assign({}, state, { count7: action.payload }),
-  [SET_COUNT8]: (state, action) => Object.assign({}, state, { count8: action.payload }),
-  [SET_COUNT9]: (state, action) => Object.assign({}, state, { count9: action.payload }),
+  [SET_COUNT]: (state , action) => Object.assign({}, state, { [action.node]: action.payload }),
   [RENEW_COUNT]: () => initialState
 };
 
